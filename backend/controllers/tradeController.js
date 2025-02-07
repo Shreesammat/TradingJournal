19dab618e3c23c3d96b42f204fb3968343f5d42e
Shreesammat Joshi <busibaka@gmail.com>
2025-02-07 23:10:19 +0530

    v3.0.0
    added trade functionalities like getTrades, createTrades

--- backend/controllers/tradeController.js
+++ backend/controllers/tradeController.js
@@ -1,7 +1,23 @@
-import express from 'express'
+import Trade from '../models/Trade';
 
 const getUserTrades = async (req, res) => {
-    
+    try {
+        const userId = req.user.id;
+
+        const trades = await Trade.find({userId: userId});
+
+        return res.status(200).json({
+            success: true,
+            trades: trades.length > 0 ? trades : [],
+            message: trades.length > 0 ? "Trades retrieved successfully!" : "No trades found!",
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+            message: "Server Error!"
+        })
+    }
 }
 
 const getTradeById = async (req, res) => {
@@ -9,7 +25,36 @@ const getTradeById = async (req, res) => {
 }
 
 const createTrade = async (req, res) => {
+    try {
+        const { entryTime, exitTime, entryPrice, exitPrice, pnl, emotions, psychology, chartScreenShots, learnings } = req.body;
 
+        const newTrade = new Trade({
+            userId: req.user._id,  // Use the logged-in user's ID
+            entryTime,
+            exitTime,
+            entryPrice,
+            exitPrice,
+            pnl,
+            emotions,
+            psychology,
+            chartScreenShots,
+            learnings
+        });
+
+        await newTrade.save();
+
+        return res.status(200).json({
+            success: true,
+            message:"Journal Created Successfully!",
+            trade: newTrade
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+            message: "Error creating trade!"
+        })
+    }
 }
 
 const editTrade = async (req, res) => {
